319224ac9829495429730e0b840fea4370523c2c
Will Kukkamalla <will.kukkamalla@mail.utoronto.ca>
2025-05-14 19:26:28 -0400

    changes to redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,9 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.cache_store =: :redis_cache_store, {
+    url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1"),
+    namespace: "cache"}
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
---
+++ config/initializers/redis.rb
@@ -0,0 +1,3 @@
+redis_url = ENV.fetch("REDIS_URL", "redis://localhost:6379/1")
+
+$redis = Redis.new(url: redis_url)
